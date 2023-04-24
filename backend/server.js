ff2273d6da426093dd1eea897197d838ba87e712
OliaK <olgamr@walla.com>
2023-04-24 22:09:08 +0300

    Adding backend

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const { append } = require('express/lib/response')
+const { chats } = require('./data/data')
+const connectDB = require('./config/db')
+const colors = require('colors')
+
+dotenv.config()
+connectDB()
+const app = express()
+connectDB()
+
+app.get('/', (req, res) => {
+  res.send('API is Runnig Sucessfully')
+})
+
+app.get('/api/chat', (req, res) => {
+  res.send(chats)
+})
+
+app.get('/api/chat/:id', (req, res) => {
+  //   console.log(req.params.id)
+  const singleChat = chats.find((c) => c._id === req.params.id)
+  res.send(singleChat)
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(5000, console.log(`Server Started on Port ${PORT}`.yellow.bold))
---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import HomePage from './pages/HomePage.jsx'
+import ChatPage from './pages/ChatPage.jsx'
+
+function App() {
+  return (
+    <div className='App'>
+      <Routes>
+        <Route
+          path='/'
+          Component={HomePage}
+        />
+        <Route
+          path='/chats'
+          Component={ChatPage}
+        />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
