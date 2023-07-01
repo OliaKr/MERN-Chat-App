2a0ff3bc208a870cffdf259bd89b3e002f625d4b
OliaK <olgamr@walla.com>
2023-07-01 23:46:04 +0300

    Deploying to Firebase

--- backend/server.js
+++ backend/server.js
@@ -21,19 +21,19 @@ app.use("/api/message", messageRoutes);
 
 // --------------------------deployment------------------------------
 
-// const __dirname1 = path.resolve();
+const __dirname1 = path.resolve();
 
-// if (process.env.ENV === "production") {
-//   app.use(express.static(path.join(__dirname1, "/frontend/build")));
+if (process.env.ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
-//   app.get("*", (req, res) =>
-//     res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
-//   );
-// } else {
-//   app.get("/", (req, res) => {
-//     res.send("API is running..");
-//   });
-// }
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
 
 // --------------------------deployment------------------------------
 
@@ -51,7 +51,10 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin:
+      //  "http://localhost:3000",
+      "https://olia-chat-app.web.app/",
+
     // credentials: true,
   },
 });
