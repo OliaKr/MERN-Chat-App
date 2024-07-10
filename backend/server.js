243a7c5b02b184808d6ad6fd62067ebecceb60d4
OliaK <olgamr@walla.com>
2024-07-10 14:34:30 +0300

    QA

--- backend/server.js
+++ backend/server.js
@@ -1,80 +1,3 @@
-// const express = require("express");
-// const connectDB = require("./config/db");
-// const dotenv = require("dotenv");
-// const userRoutes = require("./routes/userRoutes");
-// const chatRoutes = require("./routes/chatRoutes");
-// const messageRoutes = require("./routes/messageRoutes");
-// const { notFound, errorHandler } = require("./middleware/errorMiddleware");
-// const path = require("path");
-// const cors = require("cors");
-
-// dotenv.config();
-// connectDB();
-// const app = express();
-
-// // Enable CORS for all routes
-// app.use(cors());
-
-// app.use(express.json());
-
-// app.use("/api/user", userRoutes);
-// app.use("/api/chat", chatRoutes);
-// app.use("/api/message", messageRoutes);
-
-// app.get("/", (req, res) => {
-//   res.send("API is running..");
-// });
-
-// // Error Handling middlewares
-// app.use(notFound);
-// app.use(errorHandler);
-
-// const PORT = process.env.PORT;
-
-// const server = app.listen(
-//   PORT,
-//   console.log(`Server running on PORT ${PORT}...`)
-// );
-
-// const io = require("socket.io")(server, {
-//   pingTimeout: 60000,
-//   cors: {
-//     origin: "https://olia-chat-app.web.app",
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     allowedHeaders: ["Content-Type", "Authorization"],
-//   },
-// });
-
-// io.on("connection", (socket) => {
-//   console.log("Connected to socket.io");
-//   socket.on("setup", (userData) => {
-//     socket.join(userData?._id);
-//     socket.emit("connected");
-//   });
-
-//   socket.on("join chat", (room) => {
-//     socket.join(room);
-//     console.log("User Joined Room: " + room);
-//   });
-//   socket.on("typing", (room) => socket.in(room).emit("typing"));
-//   socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
-
-//   socket.on("new message", (newMessageRecieved) => {
-//     var chat = newMessageRecieved.chat;
-//     if (!chat.users) return console.log("chat.users not defined");
-
-//     chat.users.forEach((user) => {
-//       if (user == newMessageRecieved.sender?._id) return;
-//       console.log("currentUser", user, newMessageRecieved.sender?._id);
-//       socket.in(user).emit("message recieved", newMessageRecieved);
-//     });
-//   });
-
-//   socket.off("setup", () => {
-//     console.log("USER DISCONNECTED");
-//     socket.leave(userData?._id);
-//   });
-// });
 const express = require("express");
 const connectDB = require("./config/db");
 const dotenv = require("dotenv");
@@ -89,6 +12,7 @@ dotenv.config();
 connectDB();
 const app = express();
 
+// Enable CORS for all routes
 app.use(cors());
 
 app.use(express.json());
@@ -97,12 +21,11 @@ app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
 
-app.use(express.static(path.join(__dirname, "frontend", "build")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+app.get("/", (req, res) => {
+  res.send("API is running..");
 });
 
+// Error Handling middlewares
 app.use(notFound);
 app.use(errorHandler);
 
